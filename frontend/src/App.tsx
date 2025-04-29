a3b7aaa1a0d25811a4ce790167403c78cbb4f473
Mosacd <levanmosiashvili5@gmail.com>
2025-04-29 20:19:42 +0400

    most of the base functionality implemeted according to the instructions, needs some styling and minor fixes here and there

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,16 @@
-import { useState } from 'react'
+
+import PracticeView from './components/PracticeView'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
       <div>
+      <h1>Flashcard Learner</h1>
+      <PracticeView/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
